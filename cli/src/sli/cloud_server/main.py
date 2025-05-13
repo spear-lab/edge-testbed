8e5db9d4651910755de6044e4c596780558a2163
Malyuk-A <malyuk.alexander1999@gmail.com>
2025-05-13 11:43:52 +0200

    restructured files - split into awx and local (development machine/laptop) playbooks and roles

--- cli/src/sli/cloud_server/main.py
+++ cli/src/sli/cloud_server/main.py
@@ -1,50 +1,23 @@
 import ansible_runner
 import typer
-
+import getpass
 from sli.utils.auxiliary import find_repo_root, get_playbook_path
 from sli.utils.logging import logger
 from sli.utils.styling import create_spinner_context_manager
 from sli.utils.typer_augmentations import AliasGroup
 
+from sli.configuration.security.main import get_vault_pwd_file_path
+
 app = typer.Typer(cls=AliasGroup)
 
 
-@app.command("url", help="Prints the url to the AWX cluster")
-def show_url() -> None:
-    spinner_context = create_spinner_context_manager(
-        message="Fetching port information from AWX cluster"
+@app.command("initial-setup, is", help="Installs fundamental dependencies, such as apt packages, docker, etc.")
+def initial_setup() -> None:
+    become_password = getpass.getpass("[sudo] cloud-server password: ")
+    ansible_runner.run(
+        playbook=str(get_playbook_path("local/cloud-server/initial-setup.yml")),
+        private_data_dir=str(find_repo_root()),
+        cmdline=f"--vault-password-file {get_vault_pwd_file_path()} --ask-become-pass",
+        verbosity=1,
+        passwords={"^BECOME password.*:\\s*?$": become_password},
     )
-    with spinner_context:
-        result = ansible_runner.run(
-            playbook=str(get_playbook_path("awx/get_url.yml")),
-            private_data_dir=str(find_repo_root()),
-            quiet=True,
-        )
-    if result.rc == 0:
-        target_host = [k for k in result.stats.get("ok") if k != "localhost"][0]
-        facts = result.get_fact_cache(target_host)
-        url = facts.get("awx_cluster_vpn_url")
-        logger.info("The AWX Cluster URL (VPN Tunneled): \n" + url)
-    else:
-        logger.error("The ansible playbook failed - Ensure that you are connected to the VPN.")
-
-
-@app.command("admin-pwd", help="Prints the admin pwd to log into the AWX cluster")
-def show_admin_pwd() -> None:
-    spinner_context = create_spinner_context_manager(
-        message="Fetching port information from AWX cluster"
-    )
-    with spinner_context:
-        result = ansible_runner.run(
-            playbook=str(get_playbook_path("awx/get_admin_pwd.yml")),
-            private_data_dir=str(find_repo_root()),
-            quiet=True,
-        )
-    if result.rc == 0:
-        target_host = [k for k in result.stats.get("ok") if k != "localhost"][0]
-        facts = result.get_fact_cache(target_host)
-        pwd = facts.get("awx_admin_pwd")
-        logger.info("The AWX Cluster Admin PWD (VPN Tunneled): \n" + pwd)
-        logger.info("The username is 'admin'")
-    else:
-        logger.error("The ansible playbook failed - Ensure that you are connected to the VPN.")
